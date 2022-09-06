78d4ce891ed78ed3ecec7dd7d95734a20d507be7
ozlemertosun <U2407193@rts-365.dk>
2022-09-06 15:05:16 +0200

    add header for login

---
+++ app/src/assets/pages/Login.js
@@ -0,0 +1,17 @@
+const Login = () => {
+  return (
+    <div>
+      <div className="absolute top-44 z-50">
+        <h1 className="text-curry bold text-biggest leading-none tracking-tighter ml-9">
+          Believe Yourself
+        </h1>
+        <div className="flex items-center gap-2">
+          <hr className="h-[1px] border-none bg-sky w-8" />
+          <p className="text-sky bold text-smallMedium">Train like a pro</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
