f3166e4f77f609c3465e9fc1f001b06c9cb58d32
ozlemertosun <U2407193@rts-365.dk>
2022-09-06 09:41:41 +0200

    add updated style for navigation

--- app/src/assets/components/NavLinks.js
+++ app/src/assets/components/NavLinks.js
@@ -1,55 +1,28 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-const NavLinks = (props) => {
-  const animateFrom = { opacity: 0, y: -40, x: -170 };
-  const animateTo = { opacity: 1, y: 0, x: -170 };
+const NavLinks = ({ open, setOpen }) => {
   return (
-    <ul className="absolute text-center z-50 m-auto">
+    <>
       <Link to="/home">
-        <motion.li
-          title="Popular classes"
-          initial={animateFrom}
-          animate={animateTo}
-          transition={{ delay: 0.05 }}
-          onClick={() => props.isMenuOpen && props.closeMenu()}
-        >
+        <motion.li title="Popular classes" onClick={() => setOpen(!open)}>
           Home
         </motion.li>
       </Link>
       <Link to="/search">
-        <motion.li
-          title="Search"
-          initial={animateFrom}
-          animate={animateTo}
-          transition={{ delay: 0.1 }}
-          onClick={() => props.isMenuOpen && props.closeMenu()}
-        >
+        <motion.li title="Search" onClick={() => setOpen(!open)}>
           Search
         </motion.li>
       </Link>
       <Link to="/schedule">
-        <motion.li
-          title="My schedule"
-          initial={animateFrom}
-          animate={animateTo}
-          transition={{ delay: 0.2 }}
-          onClick={() => props.isMenuOpen && props.closeMenu()}
-        >
+        <motion.li title="My schedule" onClick={() => setOpen(!open)}>
           My Schedule
         </motion.li>
       </Link>
       <Link to="">
-        <motion.li
-          initial={animateFrom}
-          animate={animateTo}
-          transition={{ delay: 0.3 }}
-          onClick={() => props.isMenuOpen && props.closeMenu()}
-        >
-          Log out
-        </motion.li>
+        <motion.li onClick={() => setOpen(!open)}>Log out</motion.li>
       </Link>
-    </ul>
+    </>
   );
 };
 
